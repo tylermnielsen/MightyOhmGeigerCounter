7023f7bee07bdd76d6772cb492dbc45b922626bf
Tyler Nielsen <tylernielsenn@gmail.com>
2026-03-20 15:41:36 -0700

    some refactoring and making comply stricter with standards, autoformatting

--- GeigerCounter.cpp
+++ GeigerCounter.cpp
@@ -4,124 +4,112 @@
  * @brief GeigerCounter class function implementation.
  * @version 0.1
  * @date 2024-04-05
- * 
+ *
  */
 
 #include "GeigerCounter.h"
 
+static volatile uint32_t STATIC_GEIGER_COUNT = 0;
+
+static void counter_isr() { STATIC_GEIGER_COUNT++; }
+
 /**
- * @brief Construct a Geiger Counter object with default minimum sample period of 5000 ms. 
- * 
- * @param geigerPin Pin connected to the Geiger Counter's data or "pulse" output. Make sure that it is interrupt-enabled. 
+ * @brief Construct a Geiger Counter object with default minimum sample period
+ * of 5000 ms.
+ *
+ * @param geigerPin Pin connected to the Geiger Counter's data or "pulse"
+ * output. Make sure that it is interrupt-enabled.
  */
-GeigerCounter::GeigerCounter(int8_t geigerPin){
-    attachInterrupt(digitalPinToInterrupt(geigerPin), this->counter, FALLING);
-    this->samplePeriodStart = millis();
-    this->lastCPSMeasurement = 0; 
-    this->minimumSamplePeriod_ms = 5000; 
+GeigerCounter::GeigerCounter(int8_t geigerPin)
+    : GeigerCounter(geigerPin, 5000) {}
+
+/**
+ * @brief Construct a Geiger Counter object with specified minimum sample
+ * period.
+ *
+ * @param geigerPin Pin connected to the Geiger Counter's data or "pulse" output
+ * (make sure that it is interrupt-enabled)
+ * @param minimumSamplePeriod_ms Minimum sample period in ms for "running"
+ * functions
+ */
+GeigerCounter::GeigerCounter(int8_t geigerPin,
+                             unsigned long minimumSamplePeriod_ms) {
+  pinMode(geigerPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(geigerPin), counter_isr, FALLING);
+  this->samplePeriodStart = millis();
+  this->lastCPSMeasurement = 0;
+  this->minimumSamplePeriod_ms = minimumSamplePeriod_ms;
 }
 
 /**
- * @brief Construct a Geiger Counter object with specified minimum sample period. 
- * 
- * @param geigerPin Pin connected to the Geiger Counter's data or "pulse" output (make sure that it is interrupt-enabled)
- * @param minimumSamplePeriod_ms Minimum sample period in ms for "running" functions
+ * @brief If the empty (no param) constructor is used, this can be used to
+ * initialize. Or reinitialize after any initial constructor
+ *
+ * @param geigerPin Pin connected to the Geiger Counter's data or "pulse" output
+ * (make sure that it is interrupt-enabled)
  */
-GeigerCounter::GeigerCounter(int8_t geigerPin, unsigned long minimumSamplePeriod_ms){
-    attachInterrupt(digitalPinToInterrupt(geigerPin), this->counter, FALLING);
-    this->samplePeriodStart = millis();
-    this->lastCPSMeasurement = 0; 
-    this->minimumSamplePeriod_ms = minimumSamplePeriod_ms;
+void GeigerCounter::begin(int8_t geigerPin) { this->begin(geigerPin, 1000); }
+
+/**
+ * @brief If the empty (no params) constructor is used, this can be used to
+ * initialize. Or reinitialize after any initial constructor
+ *
+ * @param geigerPin Pin connected to the Geiger Counter's data or "pulse" output
+ * (make sure that it is interrupt-enabled)
+ * @param minimumSamplePeriod_ms Minimum sample period in ms for "running"
+ * functions
+ */
+void GeigerCounter::begin(int8_t geigerPin,
+                          unsigned long minimumSamplePeriod_ms) {
+  pinMode(geigerPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(geigerPin), counter_isr, FALLING);
+  this->refreshSample();
+  this->lastCPSMeasurement = 0;
+  this->minimumSamplePeriod_ms = minimumSamplePeriod_ms;
 }
 
 /**
- * @brief If the empty (no param) constructor is used, this can be used to initialize. Or reinitialize after any initial constructor
- * 
- * @param geigerPin Pin connected to the Geiger Counter's data or "pulse" output (make sure that it is interrupt-enabled)
- */
-void GeigerCounter::begin(int8_t geigerPin) {
-    attachInterrupt(digitalPinToInterrupt(geigerPin), this->counter, FALLING);
-    this->refreshSample();
-    this->lastCPSMeasurement = 0; 
-    this->minimumSamplePeriod_ms = 1000; 
-}
-
-/**
- * @brief If the empty (no params) constructor is used, this can be used to initialize. Or reinitialize after any initial constructor
- * 
- * @param geigerPin Pin connected to the Geiger Counter's data or "pulse" output (make sure that it is interrupt-enabled)
- * @param minimumSamplePeriod_ms Minimum sample period in ms for "running" functions
- */
-void GeigerCounter::begin(int8_t geigerPin, unsigned long minimumSamplePeriod_ms){
-    attachInterrupt(digitalPinToInterrupt(geigerPin), this->counter, FALLING);
-    this->refreshSample();
-    this->lastCPSMeasurement = 0; 
-    this->minimumSamplePeriod_ms = minimumSamplePeriod_ms;
-}
-
-/**
- * @brief Calculates CPS for the specified sample period, starting at function call. 
- * 
+ * @brief Calculates CPS for the specified sample period, starting at function
+ * call.
+ *
  * @param samplePeriod_ms Period to sample over
  * @return float - CPS
  */
-float GeigerCounter::getCPSBlocking(unsigned long samplePeriod_ms){
-    this->refreshSample();
-    delay(samplePeriod_ms);
-    
-    return this->getInstCPSRunning();
+float GeigerCounter::getCPSBlocking(unsigned long samplePeriod_ms) {
+  this->refreshSample();
+  delay(samplePeriod_ms);
+
+  return this->getInstCPSRunning();
 }
 
 /**
- * @brief Calculates dose in uSv/hr for the specified sample period, starting at function call. 
- * 
+ * @brief Calculates dose in uSv/hr for the specified sample period, starting at
+ * function call.
+ *
  * @param samplePeriod_ms Period to sample over
  * @return float - Dose (uSv/hr)
  */
-float GeigerCounter::getDoseBlocking(unsigned long samplePeriod_ms){
-    float CPS = this->getCPSBlocking(samplePeriod_ms);
+float GeigerCounter::getDoseBlocking(unsigned long samplePeriod_ms) {
+  float CPS = this->getCPSBlocking(samplePeriod_ms);
 
-    return ((CPS * 60.0) * SCALE_FACTOR) / 10000.0;
+  return this->CPStoDose(CPS);
 }
 
 /**
- * @brief Calculates CPS for the period since last CPS calculation. If the period single last CPS calculation is 
- * less than the minimum sample period (minimumSamplePeriod_ms, default 1000 ms) then the last calculated CPS 
- * measurement is returned. 
- * 
+ * @brief Calculates CPS for the period since last CPS calculation. If the
+ * period single last CPS calculation is less than the minimum sample period
+ * (minimumSamplePeriod_ms, default 1000 ms) then the last calculated CPS
+ * measurement is returned.
+ *
  * @return float - CPS
  */
 float GeigerCounter::getCPSRunning() {
-    unsigned long samplePeriod = millis() - this->samplePeriodStart;
-    float CPS = -1;
+  unsigned long samplePeriod = millis() - this->samplePeriodStart;
+  float CPS = -1;
 
-    if(samplePeriod < this->minimumSamplePeriod_ms){
-        return this->lastCPSMeasurement; 
-    }
-    else {
-        noInterrupts();
-        CPS = STATIC_GEIGER_COUNT / (samplePeriod / 1000.0);
-        STATIC_GEIGER_COUNT = 0;
-        interrupts();
-
-        this->samplePeriodStart = millis();
-        
-        this->lastCPSMeasurement = CPS; 
-        return CPS;
-    }
-    
-}
-
-/**
- * @brief Calculates CPS for the period since last CPS calculation. Does NOT check if the period
- * since last CPS calculation is more than the minimum sample period (minimumSamplePeriod_ms). 
- * Due to that, calling this function too frequently will cause inaccurate readings.  
- * 
- * @return float - CPS 
- */
-float GeigerCounter::getInstCPSRunning(){
-    unsigned long samplePeriod = millis() - this->samplePeriodStart;
-    float CPS = -1;
+  if (samplePeriod < this->minimumSamplePeriod_ms) {
+    return this->lastCPSMeasurement;
+  } else {
     noInterrupts();
     CPS = STATIC_GEIGER_COUNT / (samplePeriod / 1000.0);
     STATIC_GEIGER_COUNT = 0;
@@ -129,45 +117,87 @@ float GeigerCounter::getInstCPSRunning(){
 
     this->samplePeriodStart = millis();
 
-    this->lastCPSMeasurement = CPS; 
+    this->lastCPSMeasurement = CPS;
     return CPS;
+  }
 }
 
 /**
- * @brief Calculates dose based on CPS for the period since last CPS calcuation. Uses getCPSRunning()
- * so the minimum sample period is enforced. 
- * 
+ * @brief Calculates CPS for the period since last CPS calculation. Does NOT
+ * check if the period since last CPS calculation is more than the minimum
+ * sample period (minimumSamplePeriod_ms). Due to that, calling this function
+ * too frequently will cause inaccurate readings.
+ *
+ * @return float - CPS
+ */
+float GeigerCounter::getInstCPSRunning() {
+  unsigned long samplePeriod = millis() - this->samplePeriodStart;
+
+  float CPS = 0.0;  // Default to 0
+  if (samplePeriod == 0) {
+    return -1;  //  return -1 to indicate invalid reading
+  }
+
+  noInterrupts();
+  CPS = STATIC_GEIGER_COUNT / (samplePeriod / 1000.0);
+  STATIC_GEIGER_COUNT = 0;
+  interrupts();
+
+  this->samplePeriodStart = millis();
+
+  this->lastCPSMeasurement = CPS;
+  return CPS;
+}
+
+/**
+ * @brief Calculates dose based on CPS for the period since last CPS
+ * calculation. Uses getCPSRunning() so the minimum sample period is enforced.
+ *
  * @return float - Dose (uSv/hr)
  */
 float GeigerCounter::getDoseRunning() {
-    float CPS = this->getCPSRunning();
+  float CPS = this->getCPSRunning();
 
-    return ((CPS * 60.0) * SCALE_FACTOR) / 10000.0;
+  return this->CPStoDose(CPS);
 }
 
 /**
- * @brief Calculates dose based on CPS for the period since last CPS calcuation. Uses getInstCPSRunning()
- * so the minimum sample period is NOT enforced. 
- * 
+ * @brief Calculates dose based on CPS for the period since last CPS
+ * calculation. Uses getInstCPSRunning() so the minimum sample period is NOT
+ * enforced. Calling getInstDoseRunning immediately after getInstCPSRunning will
+ * result in bad readings (likely 0) as a getInstCPSRunning call will refresh
+ * the count to 0
+ *
  * @return float - Dose (uSv/hr)
  */
 float GeigerCounter::getInstDoseRunning() {
-    float CPS = this->getInstCPSRunning();
+  float CPS = this->getInstCPSRunning();
 
-    return ((CPS * 60.0) * SCALE_FACTOR) / 10000.0;
+  return this->CPStoDose(CPS);
 }
 
 /**
- * @brief Refreshes sample period, resetting STATIC_GEIGER_COUNT and samplePeriodStart.
- * 
+ * @brief Refreshes sample period, resetting STATIC_GEIGER_COUNT and
+ * samplePeriodStart.
+ *
  */
-void GeigerCounter::refreshSample(){
+void GeigerCounter::refreshSample() {
+  noInterrupts();
 
-    noInterrupts();
+  STATIC_GEIGER_COUNT = 0;
 
-    STATIC_GEIGER_COUNT = 0;
+  interrupts();
 
-    interrupts();
+  this->samplePeriodStart = millis();
+}
 
-    this->samplePeriodStart = millis();
+/**
+ * @brief Converts a given CPS value to Dose (uS/hr) using equation taken from
+ * the firmware for the ATtiny on the Mighty Ohm Geiger Counter board
+ *
+ * @param cps Counts Per Second
+ * @return float Dose (uS/hr)
+ */
+float GeigerCounter::CPStoDose(float cps) {
+  return ((cps * 60.0) * SCALE_FACTOR) / 10000.0;
 }
--- GeigerCounter.h
+++ GeigerCounter.h
@@ -1,50 +1,49 @@
 /**
  * @file GeigerCounter.h
  * @author Tyler Nielsen
- * @brief GeigerCounter class definition for interfacing with the "pulse" out of a Mighty Ohm Geiger Counter.
+ * @brief GeigerCounter class definition for interfacing with the "pulse" out of
+ * a Mighty Ohm Geiger Counter.
  * @version 0.1
  * @date 2024-04-05
- *  
+ *
  */
 
 #ifndef GEIGER_COUNTER_H
 #define GEIGER_COUNTER_H
 
-#include "Arduino.h"
 #include <stdint.h>
+
+#include "Arduino.h"
 
 #define SCALE_FACTOR 57
 
-static volatile uint32_t STATIC_GEIGER_COUNT = 0; 
-
 class GeigerCounter {
+ private:
+  unsigned long minimumSamplePeriod_ms;
+  float lastCPSMeasurement;
+  unsigned long samplePeriodStart;
 
-    private:
-        unsigned long minimumSamplePeriod_ms; 
-        float lastCPSMeasurement;
-        unsigned long samplePeriodStart; 
-        static void counter(){
-            STATIC_GEIGER_COUNT++;
-        }
-    public:
-        GeigerCounter() {}
-        GeigerCounter(int8_t geigerPin);
-        GeigerCounter(int8_t geigerPin, unsigned long minimumSamplePeriod_ms);
-        
-        void begin(int8_t geigerPin);
-        void begin(int8_t geigerPin, unsigned long minimumSamplePeriod_ms);
+ public:
+  GeigerCounter() {}
+  GeigerCounter(int8_t geigerPin);
+  GeigerCounter(int8_t geigerPin, unsigned long minimumSamplePeriod_ms);
 
-        // blocking functions
-        float getCPSBlocking(unsigned long samplePeriod_ms);
-        float getDoseBlocking(unsigned long samplePeriod_ms);
+  void begin(int8_t geigerPin);
+  void begin(int8_t geigerPin, unsigned long minimumSamplePeriod_ms);
 
-        // running functions
-        float getCPSRunning();
-        float getInstCPSRunning();
-        float getDoseRunning();
-        float getInstDoseRunning();
-        void refreshSample(); 
+  // blocking functions
+  float getCPSBlocking(unsigned long samplePeriod_ms);
+  float getDoseBlocking(unsigned long samplePeriod_ms);
 
+  // running functions
+  float getCPSRunning();
+  float getInstCPSRunning();
+  float getDoseRunning();
+  float getInstDoseRunning();
+  void refreshSample();
+
+  // utility functions
+  float CPStoDose(float cps);
 };
 
 #endif
